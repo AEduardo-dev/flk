a85c362d5d84925a92e20203a2b4ccbd51f83ab4
Angel Vega <angel@auterion.com>
2025-10-24 16:22:35 +0200

    improve command runner to output success but not fail

--- src/nix/mod.rs
+++ src/nix/mod.rs
@@ -1,19 +1,18 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use std::process::Command;
 
 /// Execute a nix command
-pub fn run_nix_command(args: &[&str]) -> Result<String> {
-    let output = Command::new("nix").args(args).output()?;
+pub fn run_nix_command(args: &[&str]) -> Result<(String, String, bool)> {
+    let output = Command::new("nix")
+        .args(args)
+        .output()
+        .context("Failed to execute nix command")?;
 
-    if !output.status.success() {
-        anyhow::bail!(
-            "Nix command  nix {} failed: {}",
-            args.join(" "),
-            String::from_utf8_lossy(&output.stderr)
-        );
-    }
+    let stdout = String::from_utf8(output.stdout)?;
+    let stderr = String::from_utf8(output.stderr)?;
+    let success = output.status.success();
 
-    Ok(String::from_utf8(output.stdout)?)
+    Ok((stdout, stderr, success))
 }
 
 /// Check if nix is available
