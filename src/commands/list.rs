5df435aab0500e3a747c1c5d7d2ef90abac57fb8
AEduardo-dev <vegconed@gmail.com>
2025-10-27 13:37:39 +0000

    feat: utilize flake_info helper methods for printing of info

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,4 +1,3 @@
-use colored::Colorize;
 use std::path::Path;
 
 use anyhow::Result;
@@ -7,13 +6,8 @@ use crate::flake::parser;
 
 pub fn run_list() -> Result<()> {
     let flake_path = Path::new("flake.nix");
-    let mut flake_info = parser::parse_flake(flake_path.to_str().unwrap())?;
+    let flake_info = parser::parse_flake(flake_path.to_str().unwrap())?;
 
-    println!("{}", "Packages list:".cyan());
-    flake_info.packages.sort();
-
-    for elm in flake_info.packages {
-        println!("    {}", elm.green());
-    }
+    flake_info.display_packages();
     Ok(())
 }
