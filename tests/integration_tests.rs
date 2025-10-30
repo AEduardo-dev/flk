8d8e86a7f0a0c63a40b7de2c4bc61f3ba6c7ec4d
Angel Eduardo Vega Conde <56698379+AEduardo-dev@users.noreply.github.com>
2025-10-30 11:59:14 +0100

    fix: make version command report cargo information (#44)
    * fix: make version command report cargo information
    
    * chore: update tests to take cargo version

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -18,7 +18,7 @@ fn test_version() {
     cmd.arg("--version")
         .assert()
         .success()
-        .stdout(predicate::str::contains("0.1.0"));
+        .stdout(predicate::str::contains(env!("CARGO_PKG_VERSION")));
 }
 
 #[test]
