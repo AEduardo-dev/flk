7fed2bb0282c7252ec73b6acb9fe187ac54c09da
AEduardo-dev <vegconed@gmail.com>
2025-11-24 19:06:29 +0100

    feat: add auto import all files in the directory non-recursively

---
+++ templates/profiles/default.nix
@@ -0,0 +1,13 @@
+{pkgs}: let
+  # Auto-import all .nix files in this directory
+  entries = builtins.readDir ./.;
+  nixFiles =
+    builtins.filter
+    (name: name != "default.nix" && pkgs.lib.hasSuffix ".nix" name)
+    (builtins.attrNames entries);
+in
+  builtins.listToAttrs (map (file: {
+      name = pkgs.lib.removeSuffix ".nix" file;
+      value = import (./${file}) {inherit pkgs;};
+    })
+    nixFiles)
