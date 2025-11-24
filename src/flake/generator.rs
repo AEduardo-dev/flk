cced60a6a8ab4c12db013437dcc44fa70bd64498
AEduardo-dev <vegconed@gmail.com>
2025-11-24 20:18:15 +0100

    feat: update init command to new dendritic approach

--- src/flake/generator.rs
+++ src/flake/generator.rs
@@ -1,10 +1,26 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 
-const GENERIC_TEMPLATE: &str = include_str!("../../templates/default_flake.nix");
-const RUST_TEMPLATE: &str = include_str!("../../templates/rust_flake.nix");
-const PYTHON_TEMPLATE: &str = include_str!("../../templates/python_flake.nix");
-const NODE_TEMPLATE: &str = include_str!("../../templates/node_flake.nix");
-const GO_TEMPLATE: &str = include_str!("../../templates/go_flake.nix");
+const ROOT_FLAKE_TEMPLATE: &str = include_str!("../../templates/flake.nix");
+const HELPER_TEMPLATE: &str = include_str!("../../templates/default.nix");
+const IMPORTER_TEMPLATE: &str = include_str!("../../templates/profiles/default.nix");
+
+const GENERIC_TEMPLATE: &str = include_str!("../../templates/profiles/base.nix");
+const RUST_TEMPLATE: &str = include_str!("../../templates/profiles/rust.nix");
+const PYTHON_TEMPLATE: &str = include_str!("../../templates/profiles/python.nix");
+const NODE_TEMPLATE: &str = include_str!("../../templates/profiles/node.nix");
+const GO_TEMPLATE: &str = include_str!("../../templates/profiles/go.nix");
+
+pub fn generate_root_flake() -> Result<String> {
+    Ok(ROOT_FLAKE_TEMPLATE.to_string())
+}
+
+pub fn generate_helper_module() -> Result<String> {
+    Ok(HELPER_TEMPLATE.to_string())
+}
+
+pub fn generate_importer_module() -> Result<String> {
+    Ok(IMPORTER_TEMPLATE.to_string())
+}
 
 pub fn generate_flake(project_type: &str) -> Result<String> {
     let template = match project_type {
