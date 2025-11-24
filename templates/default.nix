f279515dcf7d35d52a3703e3b11bb72b8bd8f63e
AEduardo-dev <vegconed@gmail.com>
2025-11-24 19:05:50 +0100

    feat: generate profile names and permutations
    default file imported from the .flk dir in managed project will check
    for all existing profiles, import them and generate the possible
    combinations

---
+++ templates/default.nix
@@ -0,0 +1,25 @@
+inputs: let
+  inherit (inputs) flake-utils nixpkgs profile-lib;
+in
+  flake-utils.lib.eachDefaultSystem (
+    system: let
+      pkgs = import nixpkgs {inherit system;};
+      profileLib = profile-lib.lib {inherit pkgs;};
+
+      # Auto-import all profiles from .flk/profiles/
+      profileFiles = builtins.readDir ./profiles;
+      profileDefinitions = builtins.listToAttrs (
+        map (file: {
+          name = pkgs.lib.removeSuffix ".nix" file;
+          value = import (./profiles + "/${file}") {inherit pkgs;};
+        })
+        (builtins.filter
+          (n: n != "default.nix" && pkgs.lib.hasSuffix ".nix" n)
+          (builtins.attrNames profileFiles))
+      );
+    in
+      profileLib.mkProfileOutputs {
+        inherit profileDefinitions;
+        maxCombinations = 3; # rust, node, rust-node, etc.
+      }
+  )
