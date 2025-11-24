0c85c581b6278c5409c81779df6d83f0f7c891e8
AEduardo-dev <vegconed@gmail.com>
2025-11-24 19:04:29 +0100

    feat: simplify base flake to follow import early approach

---
+++ templates/flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "Development environment managed by flk (dendritic)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    profile-lib.url = "github:AEduardo-dev/nix-profile-lib";
+  };
+
+  outputs = inputs: import ./.flk/default.nix inputs;
+}
