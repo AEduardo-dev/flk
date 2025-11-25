dfdbea76dc65ee6a30f2dfd78bd59ef35ac792c2
AEduardo-dev <vegconed@gmail.com>
2025-11-25 09:56:56 +0100

    feat: use signal instead of exit code for refresh and switch

--- src/helpers/hooks.sh
+++ src/helpers/hooks.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# TODO: migrate base hooks to external lib to obfuscate it from the user
+# TODO: check if custom signal could yield same result than exit code
+
 refresh() {
-    exit 100
+    kill -SIGUSR1 $PPID
+    exit 0
+}
+
+switch() {
+    echo "$1" >/tmp/devshell-action-$$
+    kill -SIGUSR1 $PPID
+    exit 0
 }
