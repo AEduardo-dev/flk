7d33aaeb36bf074fd827162458e954d44580e7aa
AEduardo-dev <vegconed@gmail.com>
2025-11-27 21:33:47 +0100

    feat: update commands for new parsers struct

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -2,16 +2,15 @@ use std::path::Path;
 
 use anyhow::{Context, Result};
 
-use crate::flake::parser;
+use crate::flake::parsers::{flake::parse_flake, utils::get_default_shell_profile};
 
 pub fn run_list() -> Result<()> {
     let flake_path = Path::new(".flk/profiles/").join(format!(
         "{}.nix",
-        parser::get_default_shell_profile()
-            .context("Could not find default shell profile (flake.nix)")?
+        get_default_shell_profile().context("Could not find default shell profile (flake.nix)")?
     ));
 
-    let flake_info = parser::parse_flake(flake_path.to_str().context("Invalid path encoding")?)?;
+    let flake_info = parse_flake(flake_path.to_str().context("Invalid path encoding")?)?;
 
     flake_info.display_packages();
     Ok(())
