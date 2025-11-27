45f66d0daa952b00b6f3696cf1f47b4ecee271eb
AEduardo-dev <vegconed@gmail.com>
2025-11-27 21:50:40 +0100

    feat: add example pinning with overlays

--- .flk/pins.nix
+++ .flk/pins.nix
@@ -1,4 +1,9 @@
 {
   stable = "github:NixOS/nixpkgs/nixos-25.05";
   rust-overlay = "github:oxalica/rust-overlay";
+
+  # Pinned package sources
+  pkgs-for-nodejs = "github:NixOS/nixpkgs/a1b2c3d4e5f6";
+  pkgs-for-postgres = "github:NixOS/nixpkgs/f6e5d4c3b2a1";
+  pkgs-for-python = "github:NixOS/nixpkgs/1234567890ab";
 }
