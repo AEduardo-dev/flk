cb805be49fb588a343340f7f1bbd3ee47636a435
AEduardo-dev <vegconed@gmail.com>
2025-12-08 14:32:13 +0100

    feat: pin packages in pins file
    overlays file shall only generate the packages for latter reference

--- templates/pins.nix
+++ templates/pins.nix
@@ -1,4 +1,9 @@
 {
-  stable = "github:NixOS/nixpkgs/nixos-25.05";
-  rust-overlay = "github:oxalica/rust-overlay";
+  sources = {
+    stable = "github:NixOS/nixpkgs/nixos-25.05";
+    rust-overlay = "github:oxalica/rust-overlay";
+  };
+
+  pinnedPackages = {
+  };
 }
