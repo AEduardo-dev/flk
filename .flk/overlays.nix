e763d0011513b88569dbb3d45cfa21fa188bd6ee
AEduardo-dev <vegconed@gmail.com>
2025-12-08 13:22:37 +0100

    chore: remove test values

--- .flk/overlays.nix
+++ .flk/overlays.nix
@@ -9,8 +9,6 @@ system: let
   # Define which packages to pin from which nixpkgs version
   pinnedPackages = {
     # Key is the pin name from pins.nix, value is list of package names
-    pkgs-for-nodejs = ["nodejs" "nodejs_20" "yarn" "npm"];
-    pkgs-for-postgres = ["postgresql" "postgresql_15" "pgcli"];
     # Add more as needed:
     # pkgs-for-python = ["python311" "python311Packages.pip"];
   };
