9ce222edd086c296904abda9e815fc00cc2f0b66
AEduardo-dev <vegconed@gmail.com>
2025-12-11 14:43:08 +0100

    feat: update packages listing method with new parsing

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,8 +1,9 @@
+use colored::Colorize;
 use std::path::Path;
 
 use anyhow::{Context, Result};
 
-use crate::flake::parsers::{flake::parse_flake, utils::get_default_shell_profile};
+use crate::flake::parsers::{packages::parse_packages_section, utils::get_default_shell_profile};
 
 pub fn run_list() -> Result<()> {
     let flake_path = Path::new(".flk/profiles/").join(format!(
@@ -10,8 +11,22 @@ pub fn run_list() -> Result<()> {
         get_default_shell_profile().context("Could not find default shell profile (flake.nix)")?
     ));
 
-    let flake_info = parse_flake(flake_path.to_str().context("Invalid path encoding")?)?;
+    let flake_content = std::fs::read_to_string(&flake_path).context("Failed to read flake.nix")?;
+    let section = parse_packages_section(&flake_content)
+        .context("Failed to parse packages section in flake.nix")?;
+    let packages_info = section.to_packages();
 
-    flake_info.display_packages();
+    if packages_info.is_empty() {
+        println!(
+            "{} No packages found in the current profile.",
+            "✗".red().bold()
+        );
+        return Ok(());
+    }
+
+    for pkg in packages_info {
+        println!("{} {}", "•".green(), pkg.name);
+    }
+
     Ok(())
 }
