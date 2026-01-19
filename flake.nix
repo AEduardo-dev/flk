684bc8366455388e89f4c99d43b3e9f1bb764274
AEduardo-dev <vegconed@gmail.com>
2026-01-19 16:26:12 +0100

    feat: add Rust package and app outputs using flake-utils
    - Refactor  to use  for multi-system support
    - Add Rust package build using  with metadata from
    - Expose built binary as a flake app
    - Add overlay to expose the package as
    - Disable tests in the build due to sandbox limitations

--- flake.nix
+++ flake.nix
@@ -7,5 +7,36 @@
     profile-lib.url = "github:AEduardo-dev/nix-profile-lib";
   };
 
-  outputs = inputs: import ./.flk/default.nix inputs;
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    base = import ./.flk inputs;
+    cargoToml = (nixpkgs.lib.importTOML ./Cargo.toml).package;
+  in
+    nixpkgs.lib.recursiveUpdate base
+    (flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+
+      flkPackage = pkgs.rustPlatform.buildRustPackage {
+        pname = cargoToml.name;
+        version = cargoToml.version;
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        doCheck = false; # Disable tests: nixpkgs exposure is limited in sandboxed builds
+      };
+    in {
+      packages.flk = flkPackage;
+      apps.flk = {
+        type = "app";
+        program = "${flkPackage}/bin/${cargoToml.name}";
+      };
+
+      # Overlay to expose flk as pkgs.flk
+      overlay = final: prev: {
+        flk = self.packages.${final.system}.flk;
+      };
+    }));
 }
